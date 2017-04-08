f3e9b726ff1c588c26cd70b7fd92f21689bcf80b
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-04-08 08:38:43 +0200

    Fix formatting

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-'use strict'
+"use strict";
 
-const transfer = require('./lib/transfer')
+const transfer = require("./lib/transfer");
 
 const event = {
   sourceUrl: process.env.SOURCE_URL,
   sourceSQL: process.env.SOURCE_QUERY,
   destinationUrl: process.env.DESTINATION_URL,
   destinationTable: process.env.DESTINATION_TABLE
-}
+};
 
-console.log(`transfering ${event.sourceSQL}=>${event.destinationTable}`)
-transfer(event).then(() => {
-  console.log('transfer completed')
-}).catch((err) => {
-  console.log(`failed to transfer data ${err.message}`)
-})
+console.log(`transfering ${event.sourceSQL}=>${event.destinationTable}`);
+transfer(event)
+  .then(() => {
+    console.log("transfer completed");
+  })
+  .catch(err => {
+    console.log(`failed to transfer data ${err.message}`);
+  });
