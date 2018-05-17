db20b6bd3568f560ff4491933103201933219604
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-05-17 09:08:02 +0200

    Update logging

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-"use strict";
+'use strict';
 
-const transfer = require("./lib/transfer");
+const transfer = require('./lib/transfer');
 
 const event = {
   sourceUrl: process.env.SOURCE_URL,
   sourceSQL: process.env.SOURCE_QUERY,
+  sourceMapping: process.env.SOURCE_MAPPING,
   destinationUrl: process.env.DESTINATION_URL,
   destinationTable: process.env.DESTINATION_TABLE
 };
 
-console.log(`transfering ${event.sourceSQL}=>${event.destinationTable}`);
+console.log(`starting transfer`);
 transfer(event)
   .then(() => {
-    console.log("transfer completed");
+    console.log('transfer completed');
   })
   .catch(err => {
     console.log(`failed to transfer data ${err.message}`);
